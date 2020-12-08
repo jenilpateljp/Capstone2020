7fdcaf1d77a55de10467039ddbfca7f873249a44
Darshana-007 <71474120+Darshana-007@users.noreply.github.com>
2020-12-07 19:56:29 -0500

    location page and movie booking process

---
+++ process_booking.php
@@ -0,0 +1,109 @@
+
+<?php include('header.php');
+if(!isset($_SESSION['user']))
+{
+	header('location:login.php');
+}
+
+<link rel="stylesheet" href="validation/dist/css/bootstrapValidator.css"/>
+    
+<script type="text/javascript" src="validation/dist/js/bootstrapValidator.js"></script>
+  <!-- =============================================== -->
+  <?php
+    include('form.php');
+    $frm=new formBuilder;      
+  ?> 
+</div>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Movies</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Payment</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+
+<div class="content">
+	<div class="wrap">
+		<div class="content-top">
+			<h3>Payment</h3>
+            
+            
+            
+
+            
+            
+</div>
+			</div>
+			
+		<div class="clear"></div>	
+		
+	</div>
+<?php include('footer.php');?>
+
+</main>
+<?php
+    session_start();
+    extract($_POST);
+    include('config.php');
+    $_SESSION['screen']=$screen;
+    $_SESSION['seats']=$seats;
+    $_SESSION['amount']=$amount;
+    $_SESSION['date']=$date;
+    header('location:bank.php');
+?>
+<script>
+        $(document).ready(function() {
+            $('#form1').bootstrapValidator({
+            fields: { 
+            name: {
+            verbose: false,
+                validators: {notEmpty: {
+                        message: 'The Name is required and can\'t be empty'
+                    },regexp: {
+                        regexp: /^[a-zA-Z ]+$/,
+                        message: 'The Name can only consist of alphabets'
+                    } } },
+            number: {
+            verbose: false,
+                validators: {notEmpty: {
+                        message: 'The Card Number is required and can\'t be empty'
+                    },stringLength: {
+                    min: 16,
+                    max: 16,
+                    message: 'The Card Number must 16 characters long'
+                },regexp: {
+                        regexp: /^[0-9 ]+$/,
+                        message: 'Enter a valid Card Number'
+                    } } },
+            date: {
+            verbose: false,
+                validators: {notEmpty: {
+                        message: 'The Expire Date is required and can\'t be empty'
+                    } } },
+            cvv: {
+            verbose: false,
+                validators: {notEmpty: {
+                        message: 'The cvv is required and can\'t be empty'
+                    },stringLength: {
+                    min: 3,
+                    max: 3,
+                    message: 'The cvv must 3 characters long'
+                },regexp: {
+                        regexp: /^[0-9 ]+$/,
+                        message: 'Enter a valid cvv'
+                    } } }}
+            });
+            });
+
+</script>
